ac078d66bc085785b2ab668cc2f1161e616568fa
Yuto Araki <114855385+ucho456job@users.noreply.github.com>
2023-12-28 17:55:53 +0900

    Define constants (#9)

--- api/_pkg/dto/image_dto.go
+++ api/_pkg/dto/image_dto.go
@@ -5,14 +5,29 @@ type PostImageReqBody struct {
 	Keyword     string `json:"keyword" binding:"max=50"`
 }
 
+type GetImageSort string
+
+const (
+	GetImageSortLatest  GetImageSort = "latest"
+	GetImageSortPopular GetImageSort = "popular"
+)
+
 type GetImagesQuery struct {
-	Page             int      `form:"page" binding:"min=0"`
-	Keyword          string   `form:"keyword" binding:"max=50"`
-	Sort             string   `form:"sort" binding:"omitempty,oneof=latest popular"`
-	FavoriteImageIDs []string `form:"favorite_image_ids" binding:"omitempty,uuidslice"`
-	AuthCheck        bool     `form:"auth_check" binding:"omitempty"`
+	Page             int          `form:"page" binding:"min=0"`
+	Keyword          string       `form:"keyword" binding:"max=50"`
+	Sort             GetImageSort `form:"sort" binding:"omitempty"`
+	FavoriteImageIDs []string     `form:"favorite_image_ids" binding:"omitempty,uuidslice"`
+	AuthCheck        bool         `form:"auth_check" binding:"omitempty"`
 }
 
+type PatchImageReqType string
+
+const (
+	PatchImageReqTypeUsed      PatchImageReqType = "used"
+	PatchImageReqTypeReporting PatchImageReqType = "reporting"
+	PatchImageReqTypeConfirmed PatchImageReqType = "confirmed"
+)
+
 type PatchImageReqBody struct {
-	Type string `json:"type" binding:"required,oneof=used reporting confirmed"`
+	Type PatchImageReqType `json:"type" binding:"required"`
 }
